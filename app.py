d445e5a42f481725e5ba9a7a16553cab2afcafc5
zeroweb91 <zeroweb91@protonmail.com>
2018-03-21 22:56:13 -0500

    Simplify main file for now

--- app.py
+++ app.py
@@ -24,28 +24,34 @@ attributes of internal classes.
 """
 
 import os
-import sys
-import json
-import logging
-import logging.config
 
 import subcommands
 import click
+import toml
+import colorama
+
+GLOBAL_OPTIONS = [
+    click.option("--verbose", default=False, is_flag=True, help="Print all log messages to console")
+]
 
 
-def load_config(filename="config.json") -> dict:
+def global_options(func):
+    for option in reversed(GLOBAL_OPTIONS):
+        func = option(func)
+    return func
+
+
+def load_config(filename="config.toml") -> dict:
     """
-    Open a JSON file, and parse their values into a dict
-    :param filename: The config filename, defaults to $PWD/config.json
-    :return: A dictionary containing the loaded configuration
+    Open a TOML file, and parse their values into a dict
     """
     config = {}
 
     if os.path.exists(filename):
         with open(filename, "rt") as cfg:
             try:
-                config = json.load(cfg)
-            except json.JSONDecodeError:
+                config = toml.load(cfg)
+            except toml.TomlDecodeError:
                 print("Config file format is invalid! Providing defaults...")
     else:
         print("No config file found!")
@@ -58,73 +64,38 @@ def load_config(filename="config.json") -> dict:
 
 
 @click.group(context_settings=dict(help_option_names=["-h", "--help"]))
-@click.version_option(version=0.33, prog_name="kamina")
-@click.option("--verbose", default=False, is_flag=True,
-              help="Print all log messages to console")
-@click.option("--debug", default=False, is_flag=True,
-              help="Enable debugging")
-@click.option("--log", "-l", default=None, type=click.Path(),
-              help="Redirect logging location")
-@click.option("--config", "-c", default="config.json",
+@click.version_option(version=0.1, prog_name="kamina")
+@global_options
+@click.option("--config", "-c", default="config.toml",
               type=click.Path(exists=True),
               help="Specify alternate configuration file")
 @click.pass_context
-def main(ctx, verbose, debug, log, config) -> None:
+def main(ctx, verbose, config) -> None:
     """The Kamina service utility"""
-
-    logger = None
-    handlers = []
-    conf = {}
-
     conf = load_config(config)
-    if "logging" in conf:
-        logging.config.dictConfig(conf["logging"])
+    colorama.init()  # Initialize colorama
 
     # There might not have been any configuration loaded, so let's at least set
     # a baseline - these two values should at least be known.
-    if "debug" not in conf:
-        conf["debug"] = debug
     if "verbose" not in conf:
         conf["verbose"] = verbose
 
     # Now override the config file defaults with any command line options
-    if debug:
-        conf["debug"] = True
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-
-    # If you specified an alternate log location on the command-line, use that.
-    # Otherwise, use syslog, so we at least have that, even if the config file
-    # didn't specify any logging info.
-    if log is not None:
-        handlers.append(logging.FileHandler(log))
-    else:
-        # Make sure we're not setting up two syslog handlers
-        if not logger.handlers:
-            handlers.append(logging.handlers.SysLogHandler(address="/dev/log"))
-
     if verbose:
         conf["verbose"] = True
-        handlers.append(logging.StreamHandler(sys.stdout))
-
-    for handle in handlers:
-        logger.addHandler(handle)
 
     # Now, propogate the context for our sub-commands
     ctx.obj["CONF"] = conf
-    ctx.obj["LOG"] = logger
     ctx.obj["BASIC_COMMANDS"] = subcommands.BasicCommands(ctx)
-    ctx.obj["ADVANCED_COMMANDS"] = subcommands.AdvancedCommands()
+    ctx.obj["ADVANCED_COMMANDS"] = subcommands.AdvancedCommands(ctx)
 
 
 @main.command()
+@global_options
 @click.option("--download-ipfs", is_flag=True, help="Download ipfs using this script.")
 @click.pass_context
-def init(ctx, download_ipfs) -> None:
-    """Setup a new kamina-community node"""
-    verbose = ctx.obj['CONF']['verbose']
+def init(ctx, verbose, download_ipfs) -> None:
+    """Setup a new community node"""
     try:
         ctx.obj["BASIC_COMMANDS"].setup_community_node(download_ipfs)
     except Exception as error:
@@ -135,9 +106,10 @@ def init(ctx, download_ipfs) -> None:
 
 
 @main.command()
+@global_options
 @click.pass_context
-def daemon(ctx) -> None:
-    """Initialize the kamina-community daemon"""
+def daemon(ctx, verbose) -> None:
+    """Initialize a community daemon"""
     ctx.obj["ADVANCED_COMMANDS"].start_community_daemon()
 
 
