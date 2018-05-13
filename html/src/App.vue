f6405a0352b9856dd758954065932d2c978b0395
carmelo12341 <carmelo12341@disroot.org>
2018-05-12 21:52:32 -0400

    Added frontend base files

---
+++ html/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <Header />
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header"
+
+export default {
+    components: {
+        Header
+    }
+}
+</script>
+
+<style>
+body, html {
+    background-color: #F8F8F8;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 51564px;
+}
+
+#app {
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 14
+}
+
+.container /*TODO: Make responsive*/ {
+    width: 800px;
+    margin: 0 auto;
+}
+</style>
---
+++ html/src/components/community/Banner.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+    <!-- TODO: use dynamic banner image -->
+    <div id="community-banner">
+        <img src="../../assets/kernel-panic.jpg" alt="Community banner">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CommunityBanner"
+}
+</script>
+
+<style lang="css">
+    #community-banner {
+        height: 300px;
+        overflow: hidden;
+    }
+
+    #community-banner img {
+        max-width: 100%;
+    }
+</style>
